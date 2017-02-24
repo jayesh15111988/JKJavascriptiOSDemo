1a8289909694f1d11a8cad24a58ab91b1a2bc104
Jayesh Kawli <jkawli@wayfair.com>
2017-02-23 23:10:12 -0500

    Initial commit for JavaScript to iOS communication

--- JKJavascriptToiOSDemo/ViewController.swift
+++ JKJavascriptToiOSDemo/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var webView: UIWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.webView.loadHTMLString(self.htmlStringFromFile(with: "sampleHTMLCode"), baseURL: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func htmlStringFromFile(with name: String) -> String {
+        let path = Bundle.main.path(forResource: name, ofType: "html")
+        if let result = try? String(contentsOfFile: path!, encoding: String.Encoding.utf8) {
+            return result
+        }
+        return ""
     }
 
+    @IBAction func sendDataToJavaScriptButtonPressed(_ sender: Any) {
+        let firstName = self.firstNameField.text
+        let lastName = self.lastNameField.text
 
+        let inputPayload = ["firstName": firstName, "lastName": lastName]
+        let serializedData = try! JSONSerialization.data(withJSONObject: inputPayload, options: [])
+        let encodedData = String(data: serializedData, encoding: String.Encoding.utf8)
+
+        let _ = webView.stringByEvaluatingJavaScript(from: "customMessageWithDictionary('\(encodedData!)')")
+    }
+
+}
+
+extension ViewController: UIWebViewDelegate {
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        let _ = request.url?.scheme
+        let _ = request.url?.host
+        let jsonString = request.url?.fragment?.removingPercentEncoding
+        let jsonDictionary = convertToDictionary(text: jsonString)
+
+        if let jsonDictionary = jsonDictionary, let firstName = jsonDictionary["firstName"] as? String, let lastName = jsonDictionary["lastName"] as? String {
+            firstNameField.text = firstName
+            lastNameField.text = lastName
+        }
+
+        return true
+    }
+
+    // Ref: http://stackoverflow.com/a/30480777
+    func convertToDictionary(text: String?) -> [String: Any]? {
+
+        guard let text = text else {
+            return [:]
+        }
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
 }
 
